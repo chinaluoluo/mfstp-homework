c9edf4207d7f0efbe307f3de7fdc5b89a5d9c628
chinaluoluo <1923834697@qq.com>
2019-12-13 08:11:17 +0800

    24

---
+++ vue-start/src/components/Btn.vue
@@ -0,0 +1,23 @@
+<template>
+ <button @click="btnclick">{{a}}</button>
+</template>
+<script>
+export default {
+  name: 'btn',
+  data () {
+    return {
+      a: 0
+    }
+  },
+  methods: {
+    btnclick: function () {
+      this.a++
+      this.$emit('btnclick')
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ vue-start/src/components/HelloWorld.vue
@@ -0,0 +1,32 @@
+<template>
+
+<div>
+  <span>{{count}}</span>
+  <div></div>
+  <btn @btnclick="addone"></btn>
+  <btn @btnclick="addone"></btn>
+  <btn @btnclick="addone"></btn>
+</div>
+</template>
+
+<script>
+import Btn from './Btn'
+export default {
+  name: 'hello',
+  data () {
+    return {
+      count: 0
+    }
+  },
+  methods: {
+    addone: function () {
+      this.count++
+    }
+  },
+  components: {Btn}
+}
+</script>
+
+<style scoped>
+
+</style>
